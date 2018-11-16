adddd3c93430e6606d94786a1786d4374910dd84
kanitham <krajgo123@hotmail.com>
2018-11-16 13:27:09 -0800

    Update build_chrome_linux.sh

--- build_chrome_linux.sh
+++ build_chrome_linux.sh
@@ -1,4 +1,10 @@
+# To install git ( and open ssh )
+sudo apt-get install git
 # To install linux flavor
 sudo sh crouton -r trusty -t xfce,extension,xorg,xiwi
 # To fix the missing packages
 sudo apt-get install --fix-broken
+# To install C++ along with build essential
+sudo apt-get install idle-python3.7
+#To install add-apt-repository
+sudo apt-get install software-properties-common
